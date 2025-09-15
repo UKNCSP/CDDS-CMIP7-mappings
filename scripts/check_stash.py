f540edb580a058fd0177ac7f48eeb9fb894c94c0
Jeremy Walton <jprb.walton@gmail.com>
2025-09-15 15:02:54 +0100

    Check that we found some codes in STASH entries. Don't check HadGEM for UKESM-only variables.

--- scripts/check_stash.py
+++ scripts/check_stash.py
@@ -5,6 +5,7 @@ that all STASH codes in the Expression lines are in the STASH entries, and vice 
 """
 import sys
 import re
+UKESM_only_variables = ['fco2antt', 'fco2fos', 'fco2nat']
 
 if __name__ == '__main__':
 
@@ -27,8 +28,12 @@ if __name__ == '__main__':
 
         foundSTASH = True
         foundExpression = True
+
         # The models to check.
-        model = ["UKESM1", "HadGEM3-GC31"]
+        if any(var in filename for var in UKESM_only_variables):
+            model = ["UKESM1"]
+        else:
+            model = ["UKESM1", "HadGEM3-GC31"]
         for m in model:
             print(f"Processing {m} in {filename}")
             
@@ -53,6 +58,10 @@ if __name__ == '__main__':
                 print(expressionStashCodes)
             
             # For each STASH code in the Expression lines, check if it is in the STASH entries.
+            # Record which codes are found; we'll warn if none are found.
+            # Note that this doesn't check for duplicates in the STASH entries, because we're working
+            # with from the list of codes in the Expression lines.
+            stashEntriesCodes = []
             for code in expressionStashCodes:
                 foundCode = False
                 for line in lines:
@@ -63,38 +72,48 @@ if __name__ == '__main__':
                         # Each entry line only contains one STASH code.
                         if code in line:
                             print(f"{code} found in STASH entries for {m}")
+                            stashEntriesCodes.append(code)
                             foundCode = True
                             break
                         
-                # Warning if not found.
+                # Warning if this code was not found.
                 if not foundCode:
                     print(f"*** Warning: {code} NOT FOUND in STASH entries for {m} in {filename}")
                     foundSTASH = False
             
-            # Now check that all STASH codes in the STASH entries are in the Expression lines.        
-            for line in lines:      
-                
-                # Check that it's a STASH entry line for this model.
-                if f"| {m} " in line:
-                    
-                    # Each entry line only contains one STASH code.
-                    match = re.search('m0', line)
-                    if match:
-                        istart = match.start()
-                        code = line[istart:istart+length]
-                        
-                        if code == 'm01s00i033' and usesAlev:
-                            # This is a special case.  This STASH code is used for both
-                            # 3D and 2D versions of the variable, so may not appear in
-                            # the Expression lines if the variable uses alev levels.
-                            print(f"{code} special case for alev variable {m} in {filename}")
-                        else:
-                            if code not in expressionStashCodes:
-                                # Warning if this code is not in the Expression lines.
-                                print(f"### Warning: {code} NOT FOUND in Expression lines for {m} in {filename}")
-                                foundExpression = False
-                        continue
+            # Check that we found some STASH entries.
+            if len(stashEntriesCodes) > 0:
 
+                # Check that all STASH codes in the STASH entries are in the Expression lines.
+                for line in lines:
+                    
+                    # Check that it's a STASH entry line for this model.
+                    if f"| {m} " in line:
+                        
+                        # Each entry line only contains one STASH code.
+                        match = re.search('m0', line)
+                        if match:
+                            istart = match.start()
+                            code = line[istart:istart+length]
+
+                            if code == 'm01s00i033' and usesAlev:
+                                # This is a special case.  This STASH code is used for both
+                                # 3D and 2D versions of the variable, so may not appear in
+                                # the Expression lines if the variable uses alev levels.
+                                print(f"{code} special case for alev variable {m} in {filename}")
+                            else:
+                                if code not in expressionStashCodes:
+                                    # Warning if this code is not in the Expression lines.
+                                    print(f"### Warning: {code} NOT FOUND in Expression lines for {m} in {filename}")
+                                    foundExpression = False
+                            continue
+            else:
+                # There aren't any STASH entries, so there aren't any matches with
+                # the Expression lines.
+                print(f"*** Warning: No STASH entries found for {m} in {filename}")
+                foundExpression = False
+
+        # Summary of results for this variable.
         if foundSTASH:
             print(f"Codes in Expression all found in STASH list ", end='')
             if foundExpression:
