f7b9833b2d2788a5b32ef0e8a5aeaa3fcf5d364e
Jeremy Walton <jprb.walton@gmail.com>
2025-09-25 15:38:16 +0100

    Output subset of variables for one model.

--- scripts/make_namelist.py
+++ scripts/make_namelist.py
@@ -2,7 +2,6 @@ import csv
 import sys
 
 if __name__ == '__main__':
-
     
     if len(sys.argv) != 2:
         print(f"{sys.argv[0]} csv filename")
@@ -12,6 +11,8 @@ if __name__ == '__main__':
     with open(filename, 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         data = [row for row in reader]
-        
-    print(data[0])
     
+    for d in data[:]:
+        if d['Model'] == 'HadGEM3-GC31' and d['approved']:
+            print(d['STASH'], d['domain_profile'], d['Section'], d['Item'],
+              d['time_profile'], d['usage_profile'])
