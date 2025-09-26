d002d2cb50df06dfcc2c1109ce8dd19f876ddb04
Jeremy Walton <jprb.walton@gmail.com>
2025-09-26 17:17:05 +0100

    Add comments.

--- scripts/make_namelist.py
+++ scripts/make_namelist.py
@@ -1,3 +1,9 @@
+# Read in a CSV file containing a spreadsheet of UM stash requests, and produce
+# namelist files for each of a set of models, containing all the approved requests
+# for that model.  There may be duplicates in the spreadsheet (because each stash
+# could contribute to multiple variables), so only unique requests are included
+# in the namelist files.
+
 import csv
 import sys
 
@@ -15,7 +21,6 @@ if __name__ == '__main__':
 
     # List of models to consider (names in the 'Model' column of the spreadsheet).
     models = ['HadGEM3-GC31', 'HadGEM3-GC5', 'UKESM1', 'UKESM1-3']
-
     for model in models:
 
         # Count number of entries for this model, and how many are approved.
@@ -30,14 +35,27 @@ if __name__ == '__main__':
         for d in data[:]:
             if d['Model'] == model:
                 imod += 1
+
+                # Is this entry approved?  Note that some entries have 'TRUE' in lower case.
                 if d['approved'].upper() == 'TRUE':
                     iapproved += 1
+
+                    # Add the variables we're interested in to the set of unique entries.
+                    # Note that the order of the items in the tuple is important, as it
+                    # determines the sort order when we write out the namelist file.
                     unique.add((d['STASH'], d['domain_profile'], d['Section'], d['Item'],
                         d['time_profile'], d['usage_profile']))
 
+        # Summarise what we've found, and write out the namelist file for this model.
         print(f"Found {imod} {model} entries, of which {iapproved} are approved, and {len(unique)} are unique")
         with open(f"{model}_namelist.txt", 'w') as outfile:
+
+            # We'll sort the unique entries into order of increasing STASH (because that's
+            # the first element in the list for each entry).
             for u in sorted(unique):
+
+                # Some of these values - such as the name of the namelist and the package -
+                # are currently blank.
                 outfile.write("[namelist:umstash_streq()]\n")
                 outfile.write(f"dom_name={u[1]}\n")
                 outfile.write(f"isec={u[2]}\n")
