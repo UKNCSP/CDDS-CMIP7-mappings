18084017f508059ce0ab05aa08b67480b53b5c0c
Jeremy Walton <jprb.walton@gmail.com>
2025-09-26 16:49:43 +0100

    Extend to all models.

--- scripts/make_namelist.py
+++ scripts/make_namelist.py
@@ -12,23 +12,37 @@ if __name__ == '__main__':
     with open(filename, 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         data = [row for row in reader]
-    
-    # Want to find all approved entries with unique (STASH, domain_profile, Section, Item,
-    # time_profile, usage_profile).  Use a set to avoid duplicates.
-    unique = set()
 
-    # Count number of entries for HadGEM3-GC31 and how many are approved.
-    imod = 0
-    iapproved = 0
+    # List of models to consider (names in the 'Model' column of the spreadsheet).
+    models = ['HadGEM3-GC31', 'HadGEM3-GC5', 'UKESM1', 'UKESM1-3']
 
-    for d in data[:]:
-        if d['Model'] == 'HadGEM3-GC31':
-            imod +=1
-            if d['approved'].upper() == 'TRUE':
-                iapproved += 1
-                unique.add((d['STASH'], d['domain_profile'], d['Section'], d['Item'],
-                    d['time_profile'], d['usage_profile']))
+    for model in models:
 
-    print(f"Found {imod} HadGEM2-GC31 entries, of which {iapproved} are approved, and {len(unique)} are unique")
-    for u in sorted(unique):
-        print(u)
+        # Count number of entries for this model, and how many are approved.
+        imod = 0
+        iapproved = 0
+
+        # Want to find all approved entries for this model having unique values for (STASH,
+        # domain_profile, Section, Item, time_profile, usage_profile).  Use a set to avoid
+        # duplicates.
+        unique = set()
+
+        for d in data[:]:
+            if d['Model'] == model:
+                imod += 1
+                if d['approved'].upper() == 'TRUE':
+                    iapproved += 1
+                    unique.add((d['STASH'], d['domain_profile'], d['Section'], d['Item'],
+                        d['time_profile'], d['usage_profile']))
+
+        print(f"Found {imod} {model} entries, of which {iapproved} are approved, and {len(unique)} are unique")
+        with open(f"{model}_namelist.txt", 'w') as outfile:
+            for u in sorted(unique):
+                outfile.write("[namelist:umstash_streq()]\n")
+                outfile.write(f"dom_name={u[1]}\n")
+                outfile.write(f"isec={u[2]}\n")
+                outfile.write(f"item={u[3]}\n")
+                outfile.write(f"package=''\n")
+                outfile.write(f"tim_name={u[4]}\n")
+                outfile.write(f"use_name={u[5]}\n")
+                outfile.write(f"\n")
