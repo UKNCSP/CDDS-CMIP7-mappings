2985b68b132554b6e760806ab6e8048fcfb3faf4
Jeremy Walton <jprb.walton@gmail.com>
2025-10-01 14:54:49 +0100

    Read addresses in from external file and use them in the outputted namelist.

--- scripts/make_namelist.py
+++ scripts/make_namelist.py
@@ -9,6 +9,13 @@ import sys
 
 if __name__ == '__main__':
     
+    # Get all the lines from the addresses file.
+    filename = "addresses.txt"
+    with open(filename, 'r') as fh:
+        addresses = fh.readlines()
+    naddresses = len(addresses)
+    print(f"Read {naddresses} lines from {filename}")
+
     if len(sys.argv) != 2:
         print(f"{sys.argv[0]} csv filename")
         sys.exit(1)
@@ -22,6 +29,8 @@ if __name__ == '__main__':
     # List of models to consider (names in the 'Model' column of the spreadsheet).
     models = ['HadGEM3-GC31', 'HadGEM3-GC5', 'UKESM1', 'UKESM1-3']
     for model in models:
+
+        iaddress = 0
 
         # Count number of entries for this model, and how many are approved.
         imod = 0
@@ -56,13 +65,21 @@ if __name__ == '__main__':
             # the first element in the list for each entry).
             for u in sorted(unique):
 
-                # Some of these values - such as the name of the namelist and the package -
-                # are currently blank.
-                outfile.write("[namelist:umstash_streq()]\n")
-                outfile.write(f"dom_name={u[1]}\n")
-                outfile.write(f"isec={u[2]}\n")
-                outfile.write(f"item={u[3]}\n")
-                outfile.write(f"package=''\n")
-                outfile.write(f"tim_name={u[4]}\n")
-                outfile.write(f"use_name={u[5]}\n")
+                # Use the latest address as the value in umstash_streq (need to strip trailing
+                # linefeed for this string).  Note that the string values need to be quoted
+                # in the namelist, and the numeric values need to be converted to integers (they
+                # are read in as strings from the CSV file), otherwise they get output with leading
+                # zeros, which rose edit doesn't like.
+                outfile.write(f"[namelist:umstash_streq({addresses[iaddress].strip()})]\n")
+                outfile.write(f"dom_name='{u[1]}'\n")
+                outfile.write(f"isec={int(u[2])}\n")
+                outfile.write(f"item={int(u[3])}\n")
+                outfile.write(f"package='CMIP7 diagnostics'\n")
+                outfile.write(f"tim_name='{u[4]}'\n")
+                outfile.write(f"use_name='{u[5]}'\n")
                 outfile.write(f"\n")
+
+                iaddress += 1
+                if iaddress >= naddresses:
+                    print(f"*** Warning: Ran out of addresses after {iaddress} entries")
+                    break
