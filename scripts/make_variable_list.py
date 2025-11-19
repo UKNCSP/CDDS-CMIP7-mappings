964c925e628d58e515dbbc01ed7802848bcc79b5
Jeremy Walton <jprb.walton@gmail.com>
2025-11-19 16:26:08 +0000

    Add processing of realm and stream.

--- scripts/make_variable_list.py
+++ scripts/make_variable_list.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
         print(f"{sys.argv[0]} [list of issue files to read]")
         sys.exit(1)
     
+    m = "UKESM1-3"
     for filename in sys.argv[1:]:
         
         # Get all the lines from the file.
@@ -20,6 +21,28 @@ if __name__ == '__main__':
             if "| Key |" in line:
                 keyFound = True
                 key = line.split("|")[2].strip()
-                print(key)
+
+                # Extract mip and var from the key.
+                mip = key.split(".")[0]
+                var = key.split(".")[1]
         if not keyFound:
             print(f"Warning: No Key found in file {filename}")
+
+        realmFound = False
+        for line in lines:
+            # Get the Key value.
+            if "| Modeling realm |" in line:
+                realmFound = True
+                realm = line.split("|")[2].strip()
+        if not realmFound:
+            print(f"Warning: No realm found in file {filename}")
+
+        #if mip not in ["SImon", "SIday", "Omon", "Oday", "Oyr", "Odec"]:
+        if realm not in ["ocean"]:
+            streamFound = False
+            for line in lines:
+                if not streamFound:
+                    if f"| {m} |" in line:
+                        streamFound = True
+                        stream = line.split("|")[6].strip()
+                        print(f"{var}/{mip}:{stream}")
