5df75bb0b764419a395b4e6675fa2f09b9c598e7
Matthew Mizielinski <matthew.mizielinski@metoffice.gov.uk>
2025-12-02 14:41:06 +0000

    Extend mappings generation script

--- scripts/generate_mappings_files.py
+++ scripts/generate_mappings_files.py
@@ -1,4 +1,5 @@
 
+import sys
 import json
 import subprocess
 from collections import defaultdict
@@ -8,6 +9,13 @@ from dr_issue import DRIssue
 
 def main():
 
+    if len(sys.argv) != 3:
+        print(f"Usage: {sys.argv[0]} [model] [location]")
+    model = sys.argv[1]
+    location = sys.argv[2]
+
+
+    model = 'UKESM1-3'
     result = subprocess.check_output('gh issue list -L 2000 --json number,title,body,labels'.split())
 
     data = json.loads(result)
@@ -16,12 +24,14 @@ def main():
     issuecheck = defaultdict(lambda: defaultdict(list))
 
     for d in data:
+        if 'do-not-produce' in [i['name'] for i in d['labels']]:
+            continue
         x = DRIssue()
         if not d['title'].startswith('Variable'): # non variable issue
             continue
         try:
             x.read_text(d['body'])
-            mapping = x.cdds_mapping("UKESM1-3")
+            mapping = x.cdds_mapping(model)
         except (RuntimeError): # no info for model
             continue
                     
@@ -33,17 +43,30 @@ def main():
     
 
     counter = 0
-    for r in check:
-        for v in check[r]:
-            if len(set(check[r][v])) !=1:
-                counter +=1
-                print(counter, r,v, len(check[r][v]), len(set(check[r][v])))
-                
-                for c in check[r][v]:
-                    print(c)
-                print(issuecheck[r][v])
-                print("--")
-    breakpoint()
+
+    mappings_by_realm = defaultdict(list)
+    with open(f'{location}/issues.txt', 'w') as fh:
+        for r in check:
+            for v in check[r]:
+                if len(set(check[r][v])) !=1:
+                    counter +=1
+                    fh.write(f'{counter} {r} {v}  {len(check[r][v])}  {len(set(check[r][v]))}\n')
+                    
+                    for c in check[r][v]:
+                        fh.write(c)
+                        fh.write("\n")
+
+                    fh.write(' '.join([str(i) for i in issuecheck[r][v]])+"\n")
+                    fh.write("--\n")
+                else:
+                    mappings_by_realm[r].append(check[r][v][0])
+
+    for realm in mappings_by_realm:
+        with open(f"{location}/{model}_{realm}_mappings.cfg", "w") as fh:
+            for entry in sorted(mappings_by_realm[realm]):
+                fh.write(entry)
+                fh.write("\n")
+            
 
 
 
